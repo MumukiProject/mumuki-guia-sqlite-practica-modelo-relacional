0019b48ef8f4041c23322d84ce70dea31d24d429
nadia@mumuki.org <nadia@mumuki.org>
2018-01-19 11:02:17 -0500

    Mumuki Export on 2018-01-19 11:02:17 -0500

--- 00001_Selfie/extra.sql
+++ 00001_Selfie/extra.sql
@@ -1,3 +1,13 @@
-CREATE TABLE publicaciones (es_efimero BOOLEAN NOT NULL CHECK (es_efimero IN (0,1)));
+CREATE TABLE usuarios (id_usr INTEGER PRIMARY KEY, nombre_usr TEXT, foto_perfil_url TEXT);
 
-INSERT INTO publicaciones VALUES (1.0);
+CREATE TABLE publicaciones (id_publicacion INTEGER PRIMARY KEY, 
+foto_video_url TEXT, 
+duenio INTEGER NOT NULL, 
+es_temporal BOOLEAN NOT NULL CHECK (es_efimero IN (0,1)),
+FOREIGN KEY (duenio) REFERENCES usuarios(id_usr));
+
+INSERT INTO usuarios (1, "pepe", "url_pepe");
+
+INSERT INTO publicaciones (foto_video_url, duenio, es_temporal) VALUES ("lalala", 1, 0);
+
+  
