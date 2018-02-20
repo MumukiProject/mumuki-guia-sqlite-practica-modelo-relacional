028362c912c4ad62980e9cb09a7d8a9af1d7b826
nadia@mumuki.org <nadia@mumuki.org>
2018-02-20 13:34:36 -0500

    Mumuki Export on 2018-02-20 13:34:36 -0500

--- 00001_Selfie/extra.sql
+++ 00001_Selfie/extra.sql
@@ -1,6 +1,12 @@
 UPDATE publicaciones 
 SET fecha = date("2017-12-27") 
 WHERE id_publicacion NOT BETWEEN 3 AND 6;
+
+UPDATE publicaciones 
+SET fecha = date("2018-02-27") 
+WHERE id_publicacion BETWEEN 3 AND 6;
+
+
 /*
 DELETE FROM likes;
 DELETE FROM comentarios;
