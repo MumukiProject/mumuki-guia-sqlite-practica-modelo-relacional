a636e0e389a96ca7d50c02e709295e56e916eac8
nadia@mumuki.org <nadia@mumuki.org>
2018-02-20 13:28:06 -0500

    Mumuki Export on 2018-02-20 13:28:06 -0500

--- extra.sql
+++ extra.sql
@@ -4,8 +4,8 @@ CREATE TABLE usuarios (id_usuario INTEGER PRIMARY KEY,
 
 CREATE TABLE seguidores (id_seguidor INTEGER NOT NULL, 
   id_seguido INTEGER NOT NULL, 
-  FOREIGN KEY (id_seguidor) REFERENCES usuarios(id_usuario) ON DELETE CASCADE, 
-  FOREIGN KEY (id_seguido) REFERENCES usuarios(id_usuario) ON DELETE CASCADE, 
+  FOREIGN KEY (id_seguidor) REFERENCES usuarios(id_usuario), 
+  FOREIGN KEY (id_seguido) REFERENCES usuarios(id_usuario) , 
   PRIMARY KEY (id_seguidor, id_seguido));
 
 CREATE TABLE publicaciones (id_publicacion INTEGER PRIMARY KEY, 
@@ -13,19 +13,19 @@ CREATE TABLE publicaciones (id_publicacion INTEGER PRIMARY KEY,
   id_duenio INTEGER NOT NULL, 
   fecha TEXT NOT NULL,
   es_temporal INTEGER NOT NULL CHECK (es_temporal IN (0,1)),
-  FOREIGN KEY (id_duenio) REFERENCES usuarios(id_usuario) ON DELETE CASCADE);
+  FOREIGN KEY (id_duenio) REFERENCES usuarios(id_usuario));
 
 CREATE TABLE likes (id_publicacion INTEGER NOT NULL, 
   id_stalker INTEGER NOT NULL, 
   FOREIGN KEY (id_publicacion) REFERENCES publicaciones(id_publicacion) , 
-  FOREIGN KEY (id_stalker) REFERENCES usuarios(id_usuario) ON DELETE CASCADE, 
+  FOREIGN KEY (id_stalker) REFERENCES usuarios(id_usuario), 
   PRIMARY KEY (id_publicacion, id_stalker));
 
 CREATE TABLE comentarios (id_publicacion INTEGER NOT NULL, 
   id_stalker INTEGER NOT NULL, 
   comentario TEXT,
   FOREIGN KEY (id_publicacion) REFERENCES publicaciones(id_publicacion) , 
-  FOREIGN KEY (id_stalker) REFERENCES usuarios(id_usuario) ON DELETE CASCADE, 
+  FOREIGN KEY (id_stalker) REFERENCES usuarios(id_usuario) , 
   PRIMARY KEY (id_publicacion, id_stalker));
 
 INSERT INTO usuarios 
