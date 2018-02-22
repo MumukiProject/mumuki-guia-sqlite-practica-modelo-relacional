bc39322141f0a7cd2a884429c600fff8a916c4ad
nadia@mumuki.org <nadia@mumuki.org>
2018-02-22 08:39:25 -0500

    Mumuki Export on 2018-02-22 08:39:25 -0500

--- 00001_Selfie/extra.sql
+++ 00001_Selfie/extra.sql
@@ -5,7 +5,7 @@ WHERE id_publicacion NOT BETWEEN 3 AND 6;
 UPDATE publicaciones 
 SET fecha = date("2018-02-27") 
 WHERE id_publicacion BETWEEN 3 AND 6;
-*/
+
 UPDATE publicaciones 
 SET fecha =
   CASE
@@ -13,7 +13,11 @@ SET fecha =
     WHEN id_publicacion=5 THEN date("2017-01-06") 
     ELSE date("2017-03-15")  
   END;
-      
+  */
+UPDATE publicaciones SET fecha=date("2017-03-15");
+UPDATE publicaciones SET fecha=date("2017-12-27")  WHERE id_publicacion IN('1','2','8', '9');
+UPDATE publicaciones SET fecha=date("2017-01-06")  WHERE id_publicacion = 5;
+
 /*
 DELETE FROM likes;
 DELETE FROM comentarios;
